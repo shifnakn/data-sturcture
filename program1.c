b9bc74cfa5a18c54b54687b01a2bff6f266b7fdc
shifnakn <shifnakn21@gmail.com>
2025-10-21 12:05:36 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int a,b,s=0;
+printf("enter the first number:");
+scanf("%d",&a);
+printf("enter the second number:");
+scanf("%d",&b);
+s=a+b;
+printf("sum of numbers is   %d",s);
+}
